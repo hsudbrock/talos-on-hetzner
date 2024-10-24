27889470ccb4f6775024b33f3391ab629b317018
henning <github@hsudbrock.de>
2024-10-24 06:40:50 +0200

    Make kubeconfig and talosconfig readable only by owner

--- terraform/cluster/outputs.tf
+++ terraform/cluster/outputs.tf
@@ -15,9 +15,11 @@ output "control_plane_ip" {
 resource "local_file" "kubeconfig" {
   content = data.talos_cluster_kubeconfig.this.kubeconfig_raw
   filename = "kubeconfig"
+  file_permission = "0700"
 }
 
 resource "local_file" "talosconfig" {
   content = data.talos_client_configuration.this.talos_config
   filename = "talosconfig"
+  file_permission = "0700"
 }
